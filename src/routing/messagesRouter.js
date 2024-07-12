00754934c86875986df35073ac8b6dd7a138ddfb
marcos vejega <106937506+marcos2808@users.noreply.github.com>
2024-07-11 23:45:49 -0400

    cambios en el messageRouter

--- src/routing/messagesRouter.js
+++ src/routing/messagesRouter.js
@@ -3,12 +3,13 @@ import MessageController from '../controllers/messageController.js';
 import authenticate from '../middlewares/authMiddlewares.js';
 
 const messageRouter = Router();
+const messageController = new MessageController
 
 messageRouter.use(authenticate);
 
 //definicion de rutas
-messageRouter.post('/save', MessageController.save);
-messageRouter.get('/getMessages', MessageController.getMessages);
+messageRouter.post('/save', messageController.save);
+messageRouter.get('/getMessages', messageController.getMessages);
 
 export default messageRouter;
 
