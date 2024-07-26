c1b1673223337027a48d4cbf2491d27c41ccf936
Luis Romero <106447602+Luisrome01@users.noreply.github.com>
2024-07-25 22:31:54 -0400

    prueba arreglo

--- src/db/firebase-admin.js
+++ src/db/firebase-admin.js
@@ -1,9 +1,9 @@
 // src/db/firebase-admin.js
 import admin from 'firebase-admin';
-import serviceAccount from './wasa-19ede-firebase-adminsdk-tvgeh-beb2c7771f.json' assert { type: 'json' };
+import config from './config.js';
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(config),
   storageBucket: 'wasa-19ede.appspot.com' // Asegúrate de que esta es la configuración correcta
 });
 
